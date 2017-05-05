e4c52498065902453cbcfbb2a799a00b564915a1
Chintan <chintan@wohlig.com>
2017-05-05 14:58:36 +0530

    done

--- config/env/development.js
+++ config/env/development.js
@@ -21,6 +21,6 @@ module.exports = {
     //   connection: 'someMongodbServer'
     // }
     port: 1337,
-    realHost: "http://wohlig.io:1337",
+    realHost: "https://sfa.wohlig.co.in",
     emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com"]
 };
