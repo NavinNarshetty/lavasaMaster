ef06dfb06a8689c8781f268d6daf8384059da482
Supriya Bhartiya <nargis.shaikh@wohlig.com>
2017-05-15 13:40:51 +0530

    done

--- api/controllers/PayUController.js
+++ api/controllers/PayUController.js
@@ -2,8 +2,22 @@ module.exports = _.cloneDeep(require("sails-wohlig-controller"));
 var controller = {
 
     schoolPayment: function (req, res) {
-        if (req.body) {
-            PayU.schoolPayment(req.body, res.callback);
+        if (req) {
+            console.log(sails.getBaseUrl());
+            PayU.schoolPayment(req.body, function (err, httpResponse, body) {
+                console.log('err', err);
+                console.log('httpResponse', httpResponse);
+                console.log('body', req.headers.origin);
+                if (httpResponse.statusCode == 302) {
+                    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+                    res.header("Access-Control-Allow-Headers", "*");
+                    res.header('Access-Control-Allow-Credentials', true);
+                    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+                    res.redirect(httpResponse.headers.location);
+                } else {
+                    res.send(body);
+                }
+            });
         } else {
             res.json({
                 value: false,
