94579fea9da37fa979ceb1b26e87ca21787ea2c2
Supriya Bhartiya <nargis.shaikh@wohlig.com>
2017-06-01 17:59:50 +0530

    done with login and related function

--- api/controllers/LoginController.js
+++ api/controllers/LoginController.js
@@ -1,8 +1,130 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
 var controller = {
+
     login: function (req, res) {
+        var callback = function (err, data) {
+            if (err || _.isEmpty(data)) {
+                res.json({
+                    error: err,
+                    value: false
+                });
+            } else {
+                if (data) {
+                    req.session.user = data;
+                    //req.session.save();
+                    console.log("session", req.session.user);
+                    res.json({
+                        data: data,
+                        value: true
+                    });
+                } else {
+
+                    req.session.user = {};
+                    res.json({
+                        data: {},
+                        value: false
+                    });
+                }
+            }
+        };
         if (req.body) {
-            Login.login(req.body, res.callback);
+            if (req.body.sfaid && req.body.sfaid !== "" && req.body.password && req.body.password !== "" && req.body.type && req.body.type !== "") {
+                Login.login(req.body, callback);
+            } else {
+                res.json({
+                    data: "Please provide params",
+                    value: true
+                });
+            }
+        } else {
+            res.json({
+                data: "Invalid Call",
+                value: true
+            });
+        }
+    },
+    logout: function (req, res) {
+        req.session.destroy(function (err) {
+            res.json({
+                data: "Logout Successful",
+                value: true
+            });
+        });
+    },
+
+    forgotPasswordSchool: function (req, res) {
+        if (req.body) {
+            if (req.body.email && req.body.email !== "" && req.body.sfaid && req.body.sfaid !== "" && req.body.type && req.body.type !== "") {
+                if (req.body.type == "school") {
+                    Login.forgotPassword(req.body, res.callback);
+                } else {
+                    res.json({
+                        value: false,
+                        data: "Only School can Change Password"
+                    });
+                }
+            } else {
+                res.json({
+                    value: false,
+                    data: "Please provide All Details"
+                });
+            }
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Call"
+            });
+        }
+    },
+
+    forgotPasswordAthlete: function (req, res) {
+        if (req.body) {
+            if (req.body.email && req.body.email !== "" && req.body.sfaid && req.body.sfaid !== "" && req.body.type && req.body.type !== "") {
+                if (req.body.type == "athlete") {
+                    Login.forgotPassword(req.body, res.callback);
+                } else {
+                    res.json({
+                        value: false,
+                        data: "Only Athlete can Change Password"
+                    });
+                }
+            } else {
+                res.json({
+                    value: false,
+                    data: "Please provide All Details"
+                });
+            }
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Call"
+            });
+        }
+    },
+
+    changePassword: function (req, res) {
+        if (req.body) {
+            if (req.session.user) {
+                req.body._id = req.session.user._id;
+                Login.changePassword(req.body, function (err, data) {
+                    if (err) {
+                        res.json({
+                            value: false,
+                            data: err
+                        });
+                    } else {
+                        res.json({
+                            value: true,
+                            data: data
+                        });
+                    }
+                });
+            } else {
+                res.json({
+                    value: false,
+                    data: "User Not logged in"
+                });
+            }
         } else {
             res.json({
                 value: false,
@@ -10,6 +132,5 @@ var controller = {
             });
         }
     },
-
 };
 module.exports = _.assign(module.exports, controller);
--- api/services/Login.js
+++ api/services/Login.js
@@ -9,30 +9,30 @@ var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
 var model = {
 
     login: function (data, callback) {
-        if (data.type.equals("school")) {
+        if (data.type == "school") {
             Registration.findOne({
-                sfaID: data.sfaId,
+                sfaID: data.sfaid,
                 password: data.password
             }).lean().exec(function (err, found) {
                 if (err) {
                     callback(err, null);
                 } else if (_.isEmpty(found)) {
-                    callback(null, "Data is empty");
+                    callback("Incorrect Login Details", null);
                 } else {
                     callback(null, found);
                 }
 
             });
 
-        } else if (data.type.equals("athlete")) {
+        } else if (data.type == "athlete") {
             Athelete.findOne({
-                sfaId: data.sfaId,
+                sfaId: data.sfaid,
                 password: data.password
             }).lean().exec(function (err, found) {
                 if (err) {
                     callback(err, null);
                 } else if (_.isEmpty(found)) {
-                    callback(null, "Data is empty");
+                    callback("Incorrect Login Details", null);
                 } else {
                     callback(null, found);
                 }
@@ -40,10 +40,141 @@ var model = {
             });
 
         } else {
-            callback("Incorrect Login Details", null);
-
+            callback("Incorrect Type", null);
         }
 
+    },
+
+    forgotPassword: function (data, callback) {
+        if (data.type == "school") {
+            Registration.findOne({
+                sfaID: data.sfaid,
+                email: data.email
+            }).lean().exec(function (err, found) {
+                if (err) {
+                    callback(err, null);
+                } else if (_.isEmpty(found)) {
+                    callback("Incorrect User Details", null);
+                } else {
+                    var newPassword = generator.generate({
+                        length: 8,
+                        numbers: true
+                    });
+                    Registration.update({
+                        _id: found._id
+                    }, newPassword).exec(
+                        function (err, data3) {
+                            if (err) {
+                                console.log(err);
+                                callback(err, null);
+                            } else if (data3) {
+                                console.log("New password generated");
+                                var emailData = {};
+                                emailData.from = "info@sfanow.in";
+                                emailData.email = found.email;
+                                emailData.password = newPassword;
+                                emailData.filename = "forgotPassword.ejs";
+                                emailData.subject = "SFA: Thank you for registering for SFA Mumbai 2017";
+                                console.log("emaildata", emailData);
+                                Config.email(emailData, function (err, emailRespo) {
+                                    if (err) {
+                                        console.log(err);
+                                        callback(null, err);
+                                    } else if (emailRespo) {
+                                        callback(null, emailRespo);
+                                    } else {
+                                        callback(null, "Invalid data");
+                                    }
+                                });
+
+                            }
+                        });
+                }
+
+            });
+
+        } else if (data.type == "athlete") {
+            Athelete.findOne({
+                sfaId: data.sfaid,
+                email: data.email
+            }).lean().exec(function (err, found) {
+                if (err) {
+                    callback(err, null);
+                } else if (_.isEmpty(found)) {
+                    callback("Incorrect User Details", null);
+                } else {
+                    var newPassword = generator.generate({
+                        length: 8,
+                        numbers: true
+                    });
+                    Athelete.update({
+                        _id: found._id
+                    }, newPassword).exec(
+                        function (err, data3) {
+                            if (err) {
+                                console.log(err);
+                                callback(err, null);
+                            } else if (data3) {
+                                console.log("New password generated");
+                                var emailData = {};
+                                emailData.from = "info@sfanow.in";
+                                emailData.email = found.email;
+                                emailData.password = newPassword;
+                                emailData.filename = "forgotPassword.ejs";
+                                emailData.subject = "SFA: Thank you for registering for SFA Mumbai 2017";
+                                console.log("emaildata", emailData);
+                                Config.email(emailData, function (err, emailRespo) {
+                                    if (err) {
+                                        console.log(err);
+                                        callback(null, err);
+                                    } else if (emailRespo) {
+                                        callback(null, emailRespo);
+                                    } else {
+                                        callback(null, "Invalid data");
+                                    }
+                                });
+
+                            }
+                        });
+                }
+
+            });
+
+        } else {
+            callback("Incorrect Type", null);
+        }
+
+    },
+
+    changePassword: function (data, callback) {
+        if (data.password && data.password != "" && data.oldPassword && data.oldPassword != "") {
+            if (data.password != data.oldPassword) {
+                User.findOneAndUpdate({
+                    _id: data._id,
+                    password: data.oldPassword
+                }, {
+                    password: data.password
+                }, function (err, data1) {
+                    if (err) {
+                        callback(null, {
+                            error: err
+                        });
+                    } else if (data1) {
+                        callback(null, data1);
+                    } else {
+                        callback(null, {
+                            error: "Invalid data"
+                        });
+                    }
+                });
+            } else {
+                callback(null, {
+                    error: "Password match or Same password exist"
+                });
+            }
+        } else {
+            callback(null, "Invalid data");
+        }
     },
 
 };
