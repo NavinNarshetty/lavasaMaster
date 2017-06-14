21ebc42eba4f8cd2e555e257cc1f17b1a9e74e60
Rajmshah <rajwohlig@gmail.com>
2017-06-14 15:44:08 +0530

    Done minor change

--- config/env/production.js
+++ config/env/production.js
@@ -25,8 +25,8 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 1337,
-  // port: 878,
+  // port: 1337,
+  port: 878,
   // realHost: "http://sfa2.wohlig.co.in",
   realHost: "https://sfa.wohlig.co.in",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com"]
