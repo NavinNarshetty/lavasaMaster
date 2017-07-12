fe083b668648bf83810cf44e8b0317c7656d73de
ShaikhNargis1406 <shaikh.nargis@wohlig.com>
2017-07-12 17:43:00 +0530

    done with config schema and json

---
+++ api/services/ConfigProperty.js
@@ -0,0 +1,33 @@
+var schema = new Schema({
+    year: String,
+    institutionType: {
+        type: String,
+    },
+    city: String,
+    country: String,
+    area: String,
+    totalPayAmount: Number,
+    totalPayAmountInWords: String,
+    taxAmount: String,
+    taxType: String,
+    percentTax: Number,
+    reqUrl: String,
+    domainUrl: String,
+    paymentUrl: String,
+    backendUrl: String,
+    dbName: String,
+    athleteStandards: [{
+        type: String
+    }],
+    termsAndCondition: String,
+    university: String
+});
+
+schema.plugin(deepPopulate, {});
+schema.plugin(uniqueValidator);
+schema.plugin(timestamps);
+module.exports = mongoose.model('ConfigProperty', schema);
+
+var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
+var model = {};
+module.exports = _.assign(module.exports, exports, model);
