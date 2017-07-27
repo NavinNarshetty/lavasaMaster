60542e0d78cbbf80850795d7c43fbc44d8c79cc2
ipratikminat <pratik.wohlig@gmail.co>
2017-07-27 16:49:09 +0530

    tab schema

--- api/services/Match.js
+++ api/services/Match.js
@@ -1,18 +1,50 @@
+
+var mongoose = require('mongoose');
+var deepPopulate = require('mongoose-deep-populate')(mongoose);
+var uniqueValidator = require('mongoose-unique-validator');
+var timestamps = require('mongoose-timestamp');
+var validators = require('mongoose-validators');
+var monguurl = require('monguurl');
+var autoIncrement = require('mongoose-auto-increment');
+var objectid = require("mongodb").ObjectID;
+var moment = require('moment');
+var request = require("request");
+autoIncrement.initialize(mongoose);
+
 var schema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-        uniqueCaseInsensitive: true,
-        excel: {
-            name: Name
-        }
-    }
+    incrementalId:  Number,
+    sport: {
+        type: Schema.Types.ObjectId,
+    },
+    opponentsSingle: {
+        type:[Schema.Types.ObjectId],
+        ref:'Athelete'
+    },
+    opponentsTeam: {
+        type:[Schema.Types.ObjectId],
+        ref:'TeamSport'
+    },
+    prevMatch: {
+        type: Schema.Types.ObjectId,
+        ref: 'Match'
+    },
+    nextMatch: {
+        type: Schema.Types.ObjectId,
+        ref: 'Match'
+    },
+    scoreCard: Schema.Types.Mixed,
+    results: Schema.Types.Mixed
 });
 
 schema.plugin(deepPopulate, {});
 schema.plugin(uniqueValidator);
 schema.plugin(timestamps);
+schema.plugin(autoIncrement.plugin, {
+    model: 'Match',
+    field: 'incrementalId',
+    startAt: 1,
+    incrementBy: 1
+});
 module.exports = mongoose.model('Match', schema);
 
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
