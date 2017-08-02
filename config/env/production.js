b45bfc20979b6859ee6c37d4a9cc17966a851f4e
Rajmshah <rajwohlig@gmail.com>
2017-08-02 18:52:07 +0530

    to test college

--- config/env/production.js
+++ config/env/production.js
@@ -28,8 +28,8 @@ module.exports = {
   port: 1337,
   // port: 878,
   // realHost: "http://sfa2.wohlig.co.in",
-  realHost: "http://testmumbaischool.sfanow.in",
-  // realHost: "http://testmumbaicollege.sfanow.in",
+  // realHost: "http://testmumbaischool.sfanow.in",
+  realHost: "http://testmumbaicollege.sfanow.in",
 
   // realHost: "https://sfa.wohlig.co.in",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com"]
