b893a5a120e01b718d0f3b471b435f8ff7c42053
sagarmulchandani <sagarmulchandani.sm@gmail.com>
2017-08-02 15:32:51 +0530

    Digital scoring createD :rabbit:

---
+++ digital/files.js
@@ -0,0 +1,52 @@
+module.exports = [
+  //JS & jQuery Libs
+  'bower_components/jquery/dist/jquery.min.js',
+  'bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+  'bower_components/flexslider/jquery.flexslider-min.js',
+  'bower_components/fancyBox/source/jquery.fancybox.js',
+  'bower_components/smart-app-banner/smart-app-banner.js',
+  "bower_components/moment/min/moment.min.js",
+
+
+  //Angular Libs
+  'bower_components/angular/angular.js',
+  'bower_components/angular-sanitize/angular-sanitize.js',
+  'bower_components/angular-animate/angular-animate.min.js',
+  'bower_components/angular-bootstrap/ui-bootstrap.min.js',
+  'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
+  'bower_components/angular-ui-router/release/angular-ui-router.min.js',
+  'bower_components/angular-flexslider/angular-flexslider.js',
+  'bower_components/angular-translate/angular-translate.js',
+  'bower_components/lodash/lodash.js',
+  'bower_components/angulartics/dist/angulartics.min.js',
+  // 'bower_components/angulartics-google-analytics/dist/angulartics-ga.min.js',
+  'bower_components/angular-ui-swiper/dist/angular-ui-swiper.js',
+  "bower_components/angular-toastr/dist/angular-toastr.js",
+  "bower_components/angular-toastr/dist/angular-toastr.tpls.js",
+  "bower_components/angular-promise-buttons/dist/angular-promise-buttons.js",
+
+  //App.js
+  'digital/js/app.js',
+
+  // Languages
+  'digital/js/languages/language.js',
+
+
+  // Directives
+  'digital/js/directives/directive.js',
+
+  // Filters
+  'digital/js/filters/filter.js',
+
+  // Services
+  'digital/js/services/template.js',
+
+  //Factories
+  'digital/js/factories/navigation.js',
+  'digital/js/factories/api.js',
+
+  // Contorllers
+  'digital/js/controllers/controller.js',
+  'digital/js/controllers/header-controller.js',
+  'digital/js/controllers/language-controller.js',
+];
