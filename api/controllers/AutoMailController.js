b6662d1ec3b7e3e6840d587ded5da8aac4e6f134
Supriya Bhartiya <nargis.shaikh@wohlig.com>
2017-08-03 18:20:30 +0530

    done with auto mail sports registration open

--- api/controllers/AutoMailController.js
+++ api/controllers/AutoMailController.js
@@ -1,3 +1,29 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {};
+var controller = {
+
+    athleteMail: function (req, res) {
+        if (req.body) {
+            AutoMail.athleteMail(req.body, res.callback);
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
+        }
+    },
+
+    schoolMail: function (req, res) {
+        if (req.body) {
+            AutoMail.schoolMail(req.body, res.callback);
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
+        }
+    },
+
+
+
+};
 module.exports = _.assign(module.exports, controller);
--- api/services/AutoMail.js
+++ api/services/AutoMail.js
@@ -8,6 +8,154 @@ module.exports = mongoose.model('AutoMail', schema);
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
 var model = {
 
+    athleteMail: function (data, callback) {
+        async.waterfall([
+                function (callback) {
+                    ConfigProperty.find().lean().exec(function (err, property) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            if (_.isEmpty(property)) {
+                                callback(null, []);
+                            } else {
+                                data.property = property[0];
+                                callback(null, data);
+                            }
+                        }
+                    });
+                },
+                function (data, callback) {
+                    // console.log("data", data);
+                    Athelete.find().lean().exec(function (err, complete) {
+                        if (err) {
+                            // console.log("err", err);
+                            callback(err, null);
+                        } else {
+                            if (_.isEmpty(complete)) {
+                                callback(null, []);
+                            } else {
+                                callback(null, complete);
+                            }
+                        }
+                    });
+
+                },
+                function (complete, callback) {
+                    async.eachSeries(complete, function (found, callback) {
+                            var emailData = {};
+                            emailData.name = found.firstName;
+                            emailData.from = "info@sfanow.in";
+                            emailData.email = found.email;
+                            emailData.city = data.property.sfaCity;
+                            emailData.year = data.property.year;
+                            emailData.type = "athelete"
+                            emailData.filename = "athleteAutoMail.ejs";
+                            emailData.subject = "SFA: Sports Registration Now Open";
+                            console.log("complete", emailData);
+                            Config.email(emailData, function (err, emailRespo) {
+                                if (err) {
+                                    console.log(err);
+                                    callback(null, err);
+                                } else if (emailRespo) {
+                                    callback(null, emailRespo);
+                                } else {
+                                    callback(null, "Invalid data");
+                                }
+                            });
+                        },
+                        function (err, emailData) {
+                            callback(null, "all mail sent");
+                        });
+                }
+
+            ],
+            function (err, results) {
+                if (err) {
+                    console.log(err);
+                    callback(null, results);
+                } else if (results) {
+                    if (_.isEmpty(results)) {
+                        callback(null, results);
+                    } else {
+                        callback(null, results);
+                    }
+                }
+            });
+    },
+
+    schoolMail: function (data, callback) {
+        async.waterfall([
+                function (callback) {
+                    ConfigProperty.find().lean().exec(function (err, property) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            if (_.isEmpty(property)) {
+                                callback(null, []);
+                            } else {
+                                data.property = property[0];
+                                callback(null, data);
+                            }
+                        }
+                    });
+                },
+                function (data, callback) {
+                    Registration.find().lean().exec(function (err, complete) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            if (_.isEmpty(complete)) {
+                                callback(null, []);
+                            } else {
+                                callback(null, complete);
+                            }
+                        }
+                    });
+
+                },
+                function (complete, callback) {
+                    async.each(complete, function (found, callback) {
+                            var emailData = {};
+                            emailData.from = "info@sfanow.in";
+                            emailData.email = found.email;
+                            emailData.city = data.property.sfaCity;
+                            emailData.year = data.property.year;
+                            emailData.type = data.property.institutionType;
+                            emailData.filename = "athleteAutoMail.ejs";
+                            emailData.subject = "SFA: Sports Registration Now Open";
+                            // console.log("emailData", emailData);
+                            // callback(null, emailData);
+                            Config.email(emailData, function (err, emailRespo) {
+                                if (err) {
+                                    console.log(err);
+                                    callback(null, err);
+                                } else if (emailRespo) {
+                                    callback(null, emailRespo);
+                                } else {
+                                    callback(null, "Invalid data");
+                                }
+                            });
+                        },
+                        function (err, emailData) {
+                            callback(null, "all mail sent");
+                        });
+                }
+
+            ],
+            function (err, results) {
+                if (err) {
+                    console.log(err);
+                    callback(null, results);
+                } else if (results) {
+                    if (_.isEmpty(results)) {
+                        callback(null, results);
+                    } else {
+                        callback(null, results);
+                    }
+                }
+            });
+    },
+
 
 
 
