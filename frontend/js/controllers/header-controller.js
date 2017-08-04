032bca888097a2f47507ded606764a9c1d424428
Rajmshah <rajwohlig@gmail.com>
2017-08-04 17:40:40 +0530

    minor change in header controller

--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -76,41 +76,44 @@ myApp.controller('headerCtrl', function ($scope, TemplateService, $rootScope, Na
         }];
     $scope.partner = // JavaScript Document
         [{
-            "img": "img/footer/p1.jpg",
-            "href": "",
-            "game": "Venue Partner"
-        }, {
-            "img": "img/footer/p6.jpg",
-            "href": "",
-            "game": "Hospital Partner"
-        }, {
-            "img": "img/footer/na1.jpg",
-            "href": "",
-            "game": "Sports Equipment Partner"
-        }, {
-            "img": "img/footer/na2.jpg",
-            "href": "",
-            "game": "Apparel Partner"
-        }, {
-            "img": "img/footer/na3.jpg",
-            "href": "",
-            "game": "Sports Surface Partner"
-        }, {
-            "img": "img/footer/na6.jpg",
-            "href": "",
-            "game": "Sports Mentorship Partner"
-        }, {
-            "img": "img/footer/na4.jpg",
-            "href": "",
-            "game": "Shooting Range Partner"
-        }, {
-            "img": "img/footer/p5.jpg",
-            "href": "",
-            "game": "Medical Partner"
-        }, {
-            "img": "img/footer/na5.jpg",
-            "href": "",
-            "game": "Event Partner "
-        }];
+                "img": "img/footer/p1.jpg",
+                "href": "",
+                "game": "Venue Partner"
+            }, {
+                "img": "img/footer/p6.jpg",
+                "href": "",
+                "game": "Hospital Partner"
+            },
+            // {
+            //     "img": "img/footer/na1.jpg",
+            //     "href": "",
+            //     "game": "Sports Equipment Partner"
+            // }, {
+            //     "img": "img/footer/na2.jpg",
+            //     "href": "",
+            //     "game": "Apparel Partner"
+            // },
+            {
+                "img": "img/footer/na3.jpg",
+                "href": "",
+                "game": "Sports Surface Partner"
+            }, {
+                "img": "img/footer/na6.jpg",
+                "href": "",
+                "game": "Sports Mentorship Partner"
+            }, {
+                "img": "img/footer/na4.jpg",
+                "href": "",
+                "game": "Shooting Range Partner"
+            }, {
+                "img": "img/footer/p5.jpg",
+                "href": "",
+                "game": "Medical Partner"
+            }, {
+                "img": "img/footer/na5.jpg",
+                "href": "",
+                "game": "Event Partner "
+            }
+        ];
 
 });
