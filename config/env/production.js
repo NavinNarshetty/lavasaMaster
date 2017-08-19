491fdc84f8b38ed050cd31ca0750ffe8f4311f81
Rajmshah <rajwohlig@gmail.com>
2017-08-19 16:52:44 +0530

    minor change

--- config/env/production.js
+++ config/env/production.js
@@ -25,8 +25,8 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 1337,
-  // port: 878,
+  // port: 1337,
+  port: 878,
   // realHost: "http://sfa2.wohlig.co.in",
   realHost: "http://mumbaischool.sfanow.in",
   // realHost: "http://mumbaicollege.sfanow.in",
