b717416fadce4202faabbe9722789f5483815a8c
Rajmshah <rajwohlig@gmail.com>
2017-08-23 15:36:45 +0530

    Done hiding new ui of match table page and rahul added

--- config/env/production.js
+++ config/env/production.js
@@ -42,7 +42,7 @@ module.exports = {
   // realHost: "http://testahmedabadcollege.sfanow.in",
 
   // realHost: "https://sfa.wohlig.co.in",
-  emails: ["chintan@wohlig.com", "raj@wohlig.com", "rahi.shah@sfanow.in", "shiva.singh@sfanow.in", "sunil.rathod@sfanow.in", "venkatesh.rathod@sfanow.in", "neeraj.jaiswal@sfanow.in", "rahul.singh@sfanow.in"]
+  emails: ["chintan@wohlig.com", "raj@wohlig.com", "rahi.shah@sfanow.in", "shiva.singh@sfanow.in", "sunil.rathod@sfanow.in", "venkatesh.rathod@sfanow.in", "neeraj.jaiswal@sfanow.in", "rahul.singh98612@gmail.com", "rahul.singh@sfanow.in", ]
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
