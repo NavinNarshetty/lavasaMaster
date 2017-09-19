8ded9f6b9c9d32b8641e22db01c7e08d1d0bfda8
Supriya Bhartiya <supriya.bhartiya@wohlig.com>
2017-09-19 15:30:04 +0530

    changes in standings

--- api/services/Match.js
+++ api/services/Match.js
@@ -1088,7 +1088,7 @@ var model = {
                         match.name = arr[i];
 
                         var n = matchesPerPool[name].length;
-                        console.log("n", n);
+                        console.log("n", n, "matchName", matchesPerPool[name]);
                         _.each(matchesPerPool[name], function (match) {
                             var team = {};
                             if (match.opponentsTeam[0]) {
