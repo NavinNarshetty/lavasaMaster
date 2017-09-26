829f484e3591ba1adfd453b4d1ab081351464b88
Supriya Bhartiya <supriya.bhartiya@wohlig.com>
2017-09-26 18:30:59 +0530

    done with all sport

--- api/controllers/OldSportController.js
+++ api/controllers/OldSportController.js
@@ -87,37 +87,102 @@ var controller = {
 
     setCombat: function (req, res) {
         if (req.body) {
-            // async.waterfall([
-            //     function (callback) {
-            //         OldSport.getAllFencing(req.body, function (err, category) {
-            //             if (err) {
-            //                 callback(err, null);
-            //             } else {
-            //                 if (_.isEmpty(category)) {
-            //                     callback(null, []);
-            //                 } else {
-            //                     callback(null, category);
-            //                 }
-            //             }
-            //         });
-            //     },
-            //     function (category, callback) {
-            //         OldSport.getAllRacquetDouble(req.body, function (err, category) {
-            //             if (err) {
-            //                 callback(err, null);
-            //             } else {
-            //                 if (_.isEmpty(category)) {
-            //                     callback(null, []);
-            //                 } else {
-            //                     callback(null, category);
-            //                 }
-            //             }
-            //         });
-            //     },
-            // ], function (err, found) {
-            //     res.callback(null, found);
-            // });
-            OldSport.getAllBoxing(req.body, res.callback);
+            async.waterfall([
+                function (callback) {
+                    OldSport.getAllFencing(req.body, function (err, category) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            if (_.isEmpty(category)) {
+                                callback(null, []);
+                            } else {
+                                callback(null, category);
+                            }
+                        }
+                    });
+                },
+                function (category, callback) {
+                    OldSport.getAllKata(req.body, function (err, category) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            if (_.isEmpty(category)) {
+                                callback(null, []);
+                            } else {
+                                callback(null, category);
+                            }
+                        }
+                    });
+                },
+                function (category, callback) {
+                    OldSport.getAllKumite(req.body, function (err, category) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            if (_.isEmpty(category)) {
+                                callback(null, []);
+                            } else {
+                                callback(null, category);
+                            }
+                        }
+                    });
+                },
+                function (category, callback) {
+                    OldSport.getAllJudo(req.body, function (err, category) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            if (_.isEmpty(category)) {
+                                callback(null, []);
+                            } else {
+                                callback(null, category);
+                            }
+                        }
+                    });
+                },
+                function (category, callback) {
+                    OldSport.getAllBoxing(req.body, function (err, category) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            if (_.isEmpty(category)) {
+                                callback(null, []);
+                            } else {
+                                callback(null, category);
+                            }
+                        }
+                    });
+                },
+                function (category, callback) {
+                    OldSport.getAllSportsMMA(req.body, function (err, category) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            if (_.isEmpty(category)) {
+                                callback(null, []);
+                            } else {
+                                callback(null, category);
+                            }
+                        }
+                    });
+                },
+                function (category, callback) {
+                    OldSport.getAllTaekwondo(req.body, function (err, category) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            if (_.isEmpty(category)) {
+                                callback(null, []);
+                            } else {
+                                callback(null, category);
+                            }
+                        }
+                    });
+                },
+            ], function (err, found) {
+                res.callback(null, found);
+            });
+            // OldSport.getAllKata(req.body, res.callback);
         } else {
             res.json({
                 "data": "Data not Found",
