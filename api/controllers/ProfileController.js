1a516dd3be360711aa6973161d812469d4769f0b
Supriya Bhartiya <supriya.bhartiya@wohlig.com>
2017-10-16 19:09:02 +0530

    profile seach first page done

--- api/controllers/ProfileController.js
+++ api/controllers/ProfileController.js
@@ -1,3 +1,26 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {};
+var controller = {
+    searchAthlete: function (req, res) {
+        if (req.body) {
+            Profile.searchAthlete(req.body, res.callback);
+        } else {
+            res.json({
+                "data": "Body not Found",
+                "value": false
+            })
+        }
+    },
+
+    searchSchool: function (req, res) {
+        if (req.body) {
+            Profile.searchSchool(req.body, res.callback);
+        } else {
+            res.json({
+                "data": "Body not Found",
+                "value": false
+            })
+        }
+    },
+
+};
 module.exports = _.assign(module.exports, controller);
--- api/services/Profile.js
+++ api/services/Profile.js
@@ -32,13 +32,53 @@ var model = {
         };
         var deepSearch = "school";
 
-        Athelete.find(matchObj)
+        Athelete.find(data.term)
             .sort({
                 createdAt: -1
             })
             .order(options)
             .keyword(options)
             .deepPopulate(deepSearch)
+            .page(options, function (err, found) {
+                if (err) {
+                    callback(err, null);
+                } else if (_.isEmpty(found)) {
+                    callback(null, "Data is empty");
+                } else {
+                    callback(null, found);
+                }
+            });
+    },
+
+    searchSchool: function (data, callback) {
+        var maxRow = Config.maxRow;
+
+        var page = 1;
+        if (data.page) {
+            page = data.page;
+        }
+        var field = data.field;
+        var options = {
+            field: data.field,
+            filters: {
+                keyword: {
+                    fields: ['firstName', 'middleName', 'surname', 'sfaId'],
+                    term: data.keyword
+                }
+            },
+            sort: {
+                asc: 'createdAt'
+            },
+            start: (page - 1) * maxRow,
+            count: maxRow
+        };
+        Registration.find(data.term)
+            .sort({
+                createdAt: -1
+            })
+            .order(options)
+            .keyword(options)
+            .deepPopulate()
             .page(options, function (err, found) {
                 if (err) {
                     callback(err, null);
