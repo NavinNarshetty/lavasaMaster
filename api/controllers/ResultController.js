a83822254cef81b614938e4700bfd8d84f232516
Supriya Bhartiya <supriya.bhartiya@wohlig.com>
2017-10-25 17:19:00 +0530

    chnages in result

--- api/controllers/ResultController.js
+++ api/controllers/ResultController.js
@@ -1,3 +1,25 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {};
+var controller = {
+
+    getMedalsSchool: function (req, res) {
+        if (req.body) {
+            Result.getMedalsSchool(req.body, res.callback);
+        } else {
+            res.json({
+                "data": "Body not Found",
+                "value": false
+            })
+        }
+    },
+    getRankSchool: function (req, res) {
+        if (req.body) {
+            Result.getRankSchool(req.body, res.callback);
+        } else {
+            res.json({
+                "data": "Body not Found",
+                "value": false
+            })
+        }
+    },
+};
 module.exports = _.assign(module.exports, controller);
