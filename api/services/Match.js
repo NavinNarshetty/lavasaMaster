0c7f7c734caa9933f38f8e4e4e672688c590f87b
Supriya Bhartiya <supriya.bhartiya@wohlig.com>
2017-10-26 13:42:13 +0530

    changes in winners

--- api/services/Match.js
+++ api/services/Match.js
@@ -11856,7 +11856,7 @@ var model = {
                                             if (matchData[0].resultHeat) {
                                                 result.name = n.name;
                                                 result.teamId = n.teamId;
-                                                if (!_isEmpty(n.school[0].schoolLogo)) {
+                                                if (!_.isEmpty(n.school[0].schoolLogo)) {
                                                     result.profile = n.school[0].schoolLogo;
                                                 } else {
                                                     result.profile = "";
