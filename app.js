c394bbd3da13fd47086cf9f7611db8b002acc34c
Rajmshah <rajwohlig@gmail.com>
2017-11-03 12:33:04 +0530

    database changed

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ mongoose = require('mongoose');
 // global["database"] = "sfanew";
 // global["database"] = "sfa1";
 // global["database"] = "live-sfa";
-// global["database"] = "test-sfa";
-global["database"] = "digitalScoring";
+global["database"] = "test-sfa";
+// global["database"] = "digitalScoring";
 // global["database"] = "sfa";
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/' + database, function (err) {
