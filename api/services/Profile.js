2dda25ec265245b391582c52525392a3dfb79088
Rajmshah <rajwohlig@gmail.com>
2017-11-05 23:26:05 +0530

    minor changes for finalPoints

--- api/services/Profile.js
+++ api/services/Profile.js
@@ -2305,9 +2305,27 @@ var model = {
                                         } else if (_.isEmpty(found)) {
                                             callback(null, match);
                                         } else {
+                                            var finalPoint1 = '';
+                                            var finalPoint2 = '';
                                             stats.opponentName = found.name;
                                             stats.school = found.schoolName;
                                             stats.teamId = found.teamId;
+                                            _.each(singleData.resultHockey.teams, function (n, key) {
+                                                if (n.teamResults.finalPoints !== '') {
+                                                    if (key == 0) {
+                                                        finalPoint1 = n.teamResults.finalPoints;
+                                                    } else {
+                                                        finalPoint2 = n.teamResults.finalPoints;
+                                                    }
+                                                } else {
+                                                    if (key == 0) {
+                                                        finalPoint1 = '';
+                                                    } else {
+                                                        finalPoint2 = '';
+                                                    }
+                                                }
+                                            })
+                                            stats.score = finalPoint1 + ' - ' + finalPoint2;
                                             if (!_.isEmpty(singleData.resultHockey.teams[0].teamResults.finalPoints) && !_.isEmpty(singleData.resultHockey.teams[1].teamResults.finalPoints)) {
                                                 stats.score = singleData.resultHockey.teams[0].teamResults.finalPoints + "-" + singleData.resultHockey.teams[1].teamResults.finalPoints;
                                             } else {
