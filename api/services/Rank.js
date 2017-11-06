08ac71b32176375b460a32fa9a2e8d9f3fba5286
ipratikminat <pratik.wohlig@gmail.co>
2017-11-06 16:15:21 +0530

    changes

--- api/services/Rank.js
+++ api/services/Rank.js
@@ -63,12 +63,102 @@ var model = {
         "totalMatches": -1
     },
     getSchoolByRanks: function (callback) {
-        Rank.find().sort(Rank.sortingOrder).exec(function (err, data) {
-            if(err){
-                callback(err,null);
-            }else{
-                callback(null,data);
-            }
+        Rank.find().sort(Rank.sortingOrder).lean().exec(function (err, data) {
+
+            var sportsToMerge = ['Tennis', 'Badminton', 'Table Tennis']
+            var sportsFound = [];
+            var arr = [];
+
+
+            async.concatSeries(data, function (singleData, callback) {
+
+                _.each(sportsToMerge, function (sportName, key) {
+
+
+                    singleData[sportName] = _.filter(singleData.sportData, function (sport) {
+                        console.log(sport.name.indexOf(sportName) == 0);
+                        if (sport.name.indexOf(sportName) != -1) {
+                            return sport;
+                        }
+                    })
+
+                    if (!_.isEmpty(singleData[sportName])) {
+                        var obj = {
+                            _ids:{},
+                            name : sportName,
+                            count: 0,
+                            totalCount: 0,
+                            totalPoints: 0,
+                            medals: {
+                                bronze: {
+                                    name: "bronze",
+                                    count: 0,
+                                    points: 0
+                                },
+                                silver: {
+                                    name: "silver",
+                                    count: 0,
+                                    points: 0
+                                },
+                                gold: {
+                                    name: "gold",
+                                    count: 0,
+                                    points: 0
+                                }
+                            }
+                        }
+                        _.each(singleData[sportName], function (n) {
+                            obj.count+=n.count;
+                            obj.totalCount+=n.totalCount;
+                            obj.totalPoints+=n.totalPoints;
+                            var o={};
+                            o[n.name]=n._id;
+                            console.log("o",o);
+                            obj._ids=_.assign(obj._ids,o);
+                            console.log("obj",obj);
+                            if(n && n.medals && n.medals['bronze']){
+                                obj.medals['bronze'].count +=n.medals['bronze'].count;
+                                obj.medals['bronze'].points +=n.medals['bronze'].points;
+                            }
+
+                            if(n && n.medals && n.medals['silver']){
+                                obj.medals['silver'].count +=n.medals['silver'].count;
+                                obj.medals['silver'].points +=n.medals['silver'].points;
+                            }
+
+                            if(n && n.medals && n.medals['gold']){
+                                obj.medals['gold'].count +=n.medals['gold'].count;
+                                obj.medals['gold'].points +=n.medals['gold'].points;   
+                            }
+
+                            n.removeElement = true;
+                        });
+
+                        singleData.sportData.push(obj);
+
+
+
+                    }
+                    delete singleData[sportName];
+                    singleData.sportData= _.filter(singleData.sportData, function (n) {
+                        return !n.removeElement;
+                    });
+                    if (sportsToMerge.length - 1 == key) {
+                        callback(null, singleData);
+                    }
+
+                });
+
+            }, function (err, result) {
+                callback(null, result);
+            });
+
+
+            // if(err){
+            //     callback(err,null);
+            // }else{
+            //     callback(null,data);
+            // }
         });
     }
 };
