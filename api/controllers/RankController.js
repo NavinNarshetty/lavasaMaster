ea95079ee8516e1786428d794931b8f51680a0a7
ipratikminat <pratik.wohlig@gmail.co>
2017-11-15 13:41:36 +0530

    changes

--- api/controllers/RankController.js
+++ api/controllers/RankController.js
@@ -1,19 +1,41 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
 var controller = {
-    getSchoolByRanks:function(req,res){
+    getSchoolByRanks: function (req, res) {
         Rank.getSchoolByRanks(res.callback);
     },
 
-    getSchoolBySport:function(req,res){
-        if(req.body && req.body.name){
-            var obj={
-                "name":req.body.name
+    getSchoolBySport: function (req, res) {
+        if (req.body && req.body.name) {
+            var obj = {
+                "name": req.body.name
             }
-            Rank.getSchoolBySport(obj,res.callback);
-        }else{
+            Rank.getSchoolBySport(obj, res.callback);
+        } else {
             res.json({
-                "data":"Invalid Data",
-                "value":false
+                "data": "Invalid Data",
+                "value": false
+            });
+        }
+    },
+
+    getAgeGroupsBySport: function (req, res) {
+        if (req.body.name) {
+            Rank.getAgeGroupsBySport(req.body,res.callback);
+        } else {
+            res.json({
+                data: "Insufficient Data",
+                value: false
+            });
+        }
+    },
+
+    getEventsBySport: function (req, res) {
+        if (req.body.name) {
+            Rank.getEventsBySport(req.body,res.callback);            
+        } else {
+            res.json({
+                data: "Insufficient Data",
+                value: false
             });
         }
     }
