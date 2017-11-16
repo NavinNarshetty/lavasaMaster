0267531b589084752da11126b23f59ead733a9da
Rajmshah <rajwohlig@gmail.com>
2017-11-16 08:06:02 +0530

    Doen minor chage

--- frontend/js/filters/filter.js
+++ frontend/js/filters/filter.js
@@ -1,15 +1,15 @@
 // JavaScript Document
 myApp.filter('myFilter', function () {
-    // In the return function, we must pass in a single parameter which will be the data we will work on.
-    // We have the ability to support multiple other parameters that can be passed into the filter optionally
-    return function (input, optional1, optional2) {
+        // In the return function, we must pass in a single parameter which will be the data we will work on.
+        // We have the ability to support multiple other parameters that can be passed into the filter optionally
+        return function (input, optional1, optional2) {
 
-        var output;
+            var output;
 
-        // Do filter work here
-        return output;
-    };
-})
+            // Do filter work here
+            return output;
+        };
+    })
 
     .filter('ageYearFilter', function () {
         function calculateAge(birthday) { // birthday is a date
@@ -236,7 +236,6 @@ myApp.filter('myFilter', function () {
                 other += "&style=" + style;
             }
             if (image && image !== null) {
-                console.log("adminUrl--", adminUrl2);
                 return adminUrl2 + "upload/readFile?file=" + image + other;
             } else {
                 return undefined;
