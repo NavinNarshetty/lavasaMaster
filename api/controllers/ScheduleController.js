26dfaabaa4b742db9e04275debe6389750719d31
Supriya Bhartiya <supriya.bhartiya@wohlig.com>
2017-11-23 12:14:37 +0530

    get all schedule

--- api/controllers/ScheduleController.js
+++ api/controllers/ScheduleController.js
@@ -1,3 +1,19 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {};
+var controller = {
+    getAll: function (res) {
+        Schedule.find().lean().sort({
+            createdAt: -1
+        }).exec(function (err, found) {
+            if (err) {
+                res.callback(err, null);
+            } else {
+                if (_.isEmpty(found)) {
+                    res.callback(null, []);
+                } else {
+                    res.callback(null, found);
+                }
+            }
+        });
+    },
+};
 module.exports = _.assign(module.exports, controller);
