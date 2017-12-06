378b466b5d22ac30b28903dfbae18ccdd5734305
navinnarshetty <navinnarshetty@gmail.com>
2017-12-06 11:40:29 +0530

    minor changes in header

--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -566,30 +566,33 @@ myApp.controller('footerctrl', function ($scope, TemplateService, $rootScope, Na
     $scope.mumbaiChampion = [{
         name: 'school championship',
         // mainLink: 'http://mumbaischool.sfanow.in',
-        champDetail: [{
-            name: 'about championship',
-            link: 'championship'
-        }, {
-            name: 'live updates',
-            link: 'liveupdates',
-            live: 'true'
-        }, {
-            name: 'live scores',
-            link: 'livescore',
-            score: 'live'
-        }, {
-            name: 'register',
-            link: 'register'
-        }, {
-            name: 'championship schedule',
-            link: 'championship-schedule'
-        }, {
-            name: 'draws schedule',
-            link: 'draws-schedule'
-        }, {
-            name: 'faqs',
-            link: 'faq'
-        }]
+        champDetail: [
+            //     {
+            //     name: 'about championship',
+            //     link: 'championship'
+            // }, {
+            //     name: 'live updates',
+            //     link: 'liveupdates',
+            //     live: 'true'
+            // }, {
+            //     name: 'live scores',
+            //     link: 'livescore',
+            //     score: 'live'
+            // }, {
+            //     name: 'register',
+            //     link: 'register'
+            // }, {
+            //     name: 'championship schedule',
+            //     link: 'championship-schedule'
+            // }, {
+            //     name: 'draws schedule',
+            //     link: 'draws-schedule'
+            // },
+            {
+                name: 'faqs',
+                link: 'faq'
+            }
+        ]
     }, {
         name: 'college championship',
         // mainLink: 'http://mumbaicollege.sfanow.in',
