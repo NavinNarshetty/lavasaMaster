ed2838e3ba180dce1d3e54d24ee388c225b47c5e
Suksha Khodake <sukshawohlig@gmail.com>
2017-12-28 15:52:36 +0530

    done

--- digital/js/controllers/digitalattendance.js
+++ digital/js/controllers/digitalattendance.js
@@ -79,8 +79,12 @@ myApp.controller('DigitalAttendanceCtrl', function ($scope, TemplateService, $st
     //EVENT SPORT NAME
     $scope.eventSportFun = function (eventName) {
         $scope.eventSportName = eventName;
+        if (eventName === 'Boxing' || eventName === 'Judo' || eventName === 'Kumite' || eventName === 'Taekwondo' || eventName === 'Sport MMA') {
+            $scope.showWeight = true;
+        } else {
+            $scope.showWeight = false;
+        }
     };
-
     // VIEW DRAWS
     $scope.viewDraw = function (formData, flag) {
         NavigationService.getQuickSportId(formData, function (data) {
@@ -100,30 +104,30 @@ myApp.controller('DigitalAttendanceCtrl', function ($scope, TemplateService, $st
                                 case 'Swimming 4x50m Freestyle Relay':
                                 case 'Swimming 4x50m Medley Relay':
                                 case 'Athletics 4x100m Relay':
-                                  if (flag == 'attendance') {
-                                    $state.go('attendancesheet-team', {
-                                        sport: $scope.drawDetails.sport
-                                      });
-                                    } else if (flag== 'matches'){
-                                      $state.go('creatmatch-heats', {
-                                          sport: $scope.drawDetails.sport
+                                    if (flag == 'attendance') {
+                                        $state.go('attendancesheet-team', {
+                                            sport: $scope.drawDetails.sport
+                                        });
+                                    } else if (flag == 'matches') {
+                                        $state.go('creatmatch-heats', {
+                                            sport: $scope.drawDetails.sport
                                         });
                                     }
-                                break;
+                                    break;
                                 //CASE TWO : Individual-Sport
                                 case 'Swimming':
                                 case 'Athletics':
                                 case 'Shooting':
-                                  if (flag == 'attendance') {
-                                    $state.go('attendancesheet', {
-                                        sport: $scope.drawDetails.sport
-                                    });
-                                  } else if(flag == 'matches') {
-                                    $state.go('creatmatch-heats', {
-                                        sport: $scope.drawDetails.sport
-                                      });
-                                  }
-                                  break;
+                                    if (flag == 'attendance') {
+                                        $state.go('attendancesheet', {
+                                            sport: $scope.drawDetails.sport
+                                        });
+                                    } else if (flag == 'matches') {
+                                        $state.go('creatmatch-heats', {
+                                            sport: $scope.drawDetails.sport
+                                        });
+                                    }
+                                    break;
                                 case 'Boxing':
                                 case 'Wrestling':
                                 case 'Taekwondo':
@@ -131,25 +135,25 @@ myApp.controller('DigitalAttendanceCtrl', function ($scope, TemplateService, $st
                                 case 'Sport MMA':
                                 case 'Karate':
                                     if ($scope.eventSportName != 'Kata') {
-                                      if (flag == 'attendances') {
-                                        $state.go('attendancesheet', {
-                                            sport: $scope.drawDetails.sport
-                                        });
-                                      } else if (flag == 'matches') {
-                                        $state.go('creatematch-weight');
-                                      }
+                                        if (flag == 'attendances') {
+                                            $state.go('attendancesheet', {
+                                                sport: $scope.drawDetails.sport
+                                            });
+                                        } else if (flag == 'matches') {
+                                            $state.go('creatematch-weight');
+                                        }
 
                                     } else {
                                         toastr.error('Attendance not available for this Sport');
                                     }
                                     break;
                                 default:
-                                  if (flag == 'attendance') {
-                                    toastr.error('Attendance not available for this Sport');
-                                  } else if (flag == 'matches')  {
-                                    toastr.error('Match create not available  for this Sport');
-                                  }
-                                break;
+                                    if (flag == 'attendance') {
+                                        toastr.error('Attendance not available for this Sport');
+                                    } else if (flag == 'matches') {
+                                        toastr.error('Match create not available  for this Sport');
+                                    }
+                                    break;
                             }
                         }
 
