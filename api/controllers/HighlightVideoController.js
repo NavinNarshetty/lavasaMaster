782ab73d86284c8f158e035b3f82cd39e8fc4678
Supriya Bhartiya <supriya.bhartiya@wohlig.com>
2018-01-18 15:38:28 +0530

    done with highlight thumbnail

--- api/controllers/HighlightVideoController.js
+++ api/controllers/HighlightVideoController.js
@@ -1,3 +1,15 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {};
+var controller = {
+
+    saveVideoHighlight: function (req, res) {
+        if (req.body && req.body.matchId) {
+            HighlightVideo.saveVideoHighlight(req.body, res.callback);
+        } else {
+            res.json({
+                "data": "Not Found",
+                "value": false
+            })
+        }
+    },
+};
 module.exports = _.assign(module.exports, controller);
--- api/services/HighlightVideo.js
+++ api/services/HighlightVideo.js
@@ -8,10 +8,7 @@ var schema = new Schema({
         source: String,
         link: String,
         title: String,
-        thumbnail: {
-            type: String,
-            default: ''
-        }
+        thumbnails: []
     }]
 });
 
@@ -21,5 +18,51 @@ schema.plugin(timestamps);
 module.exports = mongoose.model('HighlightVideo', schema);
 
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
-var model = {};
+var model = {
+
+    saveVideoHighlight: function (data, callback) {
+        async.waterfall([
+            function (callback) {
+                _.each(data.highlightVideo, function (n) {
+                    if (n.source === 'vimeo') {
+                        var urlData = {};
+                        urlData.videoId = n.videoLink;
+                        Vimeo.getThumnailsFromVimeo(urlData, function (err, pictures) {
+                            console.log("pictures", pictures);
+                            if (err || _.isEmpty(pictures)) {
+                                var err = "All wrong values";
+                                callback(null, {
+                                    error: err,
+                                    success: vimeoData
+                                });
+                            } else {
+                                n.thumbnails = pictures.sizes;
+                            }
+                        });
+                    }
+                });
+                callback(null, data);
+            },
+            function (data, callback) {
+                HighlightVideo.saveData(data, function (err, complete) {
+                    if (err || _.isEmpty(complete)) {
+                        callback(err, null);
+                    } else {
+                        callback(null, {
+                            error: err,
+                            success: complete
+                        });
+                    }
+                });
+            }
+        ], function (err, complete) {
+            if (err) {
+                callback(err, callback);
+            } else {
+                callback(null, complete);
+            }
+        });
+    }
+
+};
 module.exports = _.assign(module.exports, exports, model);
