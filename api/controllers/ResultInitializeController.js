acbb98ad18132e83599594895258ef6b22f5454e
Supriya Bhartiya <supriya.bhartiya@wohlig.com>
2018-03-13 18:44:54 +0530

    done with reinitialize

--- api/controllers/ResultInitializeController.js
+++ api/controllers/ResultInitializeController.js
@@ -1,3 +1,16 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {};
+var controller = {
+
+    getresultSport: function (req, res) {
+        if (req.body) {
+            ResultInitialize.getresultSport(req.body, res.callback);
+        } else {
+            res.json({
+                "data": "Body not Found",
+                "value": false
+            })
+        }
+    },
+
+};
 module.exports = _.assign(module.exports, controller);
