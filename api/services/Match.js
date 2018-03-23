0c78f5b3c0d843229f5f430dd540ee8e7e33fb64
Rajmshah <rajwohlig@gmail.com>
2018-03-23 11:01:11 +0530

    done minor changes in result combat result

--- api/services/Match.js
+++ api/services/Match.js
@@ -7607,20 +7607,25 @@ var model = {
                                                         // console.log("in volleyball");
                                                         var temp = {};
                                                         // console.log("n", n);
-                                                        resultData.teams[0].teamResults.sets[0].points = n["TEAM 1 SET 1"];
-                                                        if (n['TEAM 1 SET 2'] != undefined) {
-                                                            temp.points = n['TEAM 1 SET 2'];
-                                                            resultData.teams[0].teamResults.sets.push(temp);
+                                                        if(resultData.teams[0].teamResults){
+                                                            resultData.teams[0].teamResults.sets[0].points = n["TEAM 1 SET 1"];
+                                                            if (n['TEAM 1 SET 2'] != undefined) {
+                                                                temp.points = n['TEAM 1 SET 2'];
+                                                                resultData.teams[0].teamResults.sets.push(temp);
+                                                            }
+                                                            if (n['TEAM 1 SET 3'] != undefined) {
+                                                                temp.points = n['TEAM 1 SET 3'];
+                                                                resultData.teams[0].teamResults.sets.push(temp);
+                                                            }
+                                                            if (n['TEAM 1 SET 4'] != undefined) {
+                                                                temp.points = n['TEAM 1 SET 4'];
+                                                                resultData.teams[0].teamResults.sets.push(temp);
+                                                            }
+                                                            resultData.teams[0].teamResults.finalPoints = n['SCORE 1'];
+                                                        } else {
+                                                            resultData.teams[0].sets[0].points = n["Score 1"];
                                                         }
-                                                        if (n['TEAM 1 SET 3'] != undefined) {
-                                                            temp.points = n['TEAM 1 SET 3'];
-                                                            resultData.teams[0].teamResults.sets.push(temp);
-                                                        }
-                                                        if (n['TEAM 1 SET 4'] != undefined) {
-                                                            temp.points = n['TEAM 1 SET 4'];
-                                                            resultData.teams[0].teamResults.sets.push(temp);
-                                                        }
-                                                        resultData.teams[0].teamResults.finalPoints = n['SCORE 1'];
+
                                                     } else if (result.result.resultVar === "resultFootball") {
                                                         resultData.teams[0].teamResults.finalPoints = n['SCORE 1'];
                                                         resultData.teams[0].teamResults.penalty = n['PENALTY 1'];
@@ -7650,6 +7655,7 @@ var model = {
                                                         }
                                                     } else if (result.result.resultVar === "resultsCombat" || result.result.resultVar === "resultVolleyball" || result.result.resultVar === "resultsRacquet" || result.result.resultVar === "resultThrowball") {
                                                         var temp = {};
+                                                        if(resultData.teams[1].teamResults){
                                                         resultData.teams[1].teamResults.sets[0].points = n["TEAM 2 SET 1"];
                                                         if (n['TEAM 2 SET 2'] != undefined) {
                                                             temp.points = n['TEAM 2 SET 2'];
@@ -7664,6 +7670,9 @@ var model = {
                                                             resultData.teams[1].teamResults.sets.push(temp);
                                                         }
                                                         resultData.teams[1].teamResults.finalPoints = n['SCORE 2'];
+                                                    } else {
+                                                        resultData.teams[1].sets[0].points = n["Score 2"];
+                                                    }
                                                     } else if (result.result.resultVar === "resultFootball") {
                                                         resultData.teams[1].teamResults.finalPoints = n['SCORE 2'];
                                                         resultData.teams[1].teamResults.penalty = n['PENALTY 2'];
