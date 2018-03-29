14d6f6d33fd33a6db470fe0ce594714c34217829
Supriya Bhartiya <supriya.bhartiya@wohlig.com>
2018-03-29 15:46:57 +0530

    changes in coupon

--- api/controllers/CouponCodeController.js
+++ api/controllers/CouponCodeController.js
@@ -1,3 +1,14 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {};
+var controller = {
+    validateCode: function (req, res) {
+        if (req.body) {
+            CouponCode.validateCode(req.body, res.callback);
+        } else {
+            res.json({
+                "data": "Body not Found",
+                "value": false
+            })
+        }
+    },
+};
 module.exports = _.assign(module.exports, controller);
