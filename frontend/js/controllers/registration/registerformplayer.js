815cb1892bcb0595f089eb1d80ffead42ae5720b
sagarmulchandani <sagarmulchandani.sm@gmail.com>
2018-04-18 12:59:38 +0530

    Register old school issue seT :rabbit:

--- frontend/js/controllers/registration/registerformplayer.js
+++ frontend/js/controllers/registration/registerformplayer.js
@@ -701,6 +701,26 @@ myApp.controller('RegisterFormPlayerCtrl', function ($scope, TemplateService, $e
         NavigationService.getSchoolSFA(paramData, function (data) {
             // console.log("sfa 1", data);
             $scope.schoolList = data.data.results;
+            if ($scope.formFlag === 'edit') {
+              var findSchool = _.find($scope.schoolList, {'_id': $scope.formData.school});
+              if (findSchool == undefined || findSchool == '' || findSchool == {}) {
+                console.log("SC not in list");
+                $scope.editSchoolData = {
+                  _id: $scope.formData.school
+                }
+                $scope.urlGetSschool = 'school/getOne';
+                NavigationService.apiCallWithData($scope.urlGetSschool, $scope.editSchoolData, function(data){
+                  console.log("one", data);
+                  if (data.value == true) {
+                    $scope.schoolList.push(data.data);
+                  } else {
+                    console.log("Old School Not Found");
+                  }
+                })
+              } else {
+                console.log("Old school in schoollist");
+              }
+            }
         });
     };
 
