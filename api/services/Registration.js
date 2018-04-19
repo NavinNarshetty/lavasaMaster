79b410932380bcff1712cb88599ff5f4175ed909
Rajmshah <rajwohlig@gmail.com>
2018-04-19 18:38:35 +0530

    done emailer issues

--- api/services/Registration.js
+++ api/services/Registration.js
@@ -1891,7 +1891,8 @@ var model = {
                 emailData.receiptNo = final.accounts.transaction[len].receiptId[0];
                 emailData.paymentMode = final.accounts.transaction[len].paymentMode;
                 emailData.schoolAmount = final.accounts.transaction[len].amountPaid;
-                emailData.registrationDate = final.accounts.transaction[len].dateOfTransaction;
+                emailData.registrationDate = moment(final.accounts.transaction[len].dateOfTransaction, "DD-MM-YYYY").add(1, 'days');
+                // emailData.registrationDate = final.accounts.transaction[len].dateOfTransaction;
                 if (final.accounts.transaction[len].PayuId) {
                     emailData.transactionId = final.accounts.transaction[len].PayuId;
                 }
